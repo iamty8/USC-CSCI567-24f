378151f7ad8738ffb6f15db12cc5b34d87e301a2
Jinghy123 <109356846+Jinghy123@users.noreply.github.com>
2024-11-12 00:55:03 -0800

    Add files via upload

---
+++ scripts/run_cifar100_main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+
+NCCL_DEBUG=INFO NCCL_ASYNC_ERROR_HANDLING=1 NCCL_P2P_LEVEL=NVL CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12355 train_mp.py \
+    --dataset_name 'cifar100' \
+    --batch_size 128 \
+    --grad_from_block 11 \
+    --epochs 200 \
+    --num_workers 8 \
+    --use_ssb_splits \
+    --sup_weight 0.35 \
+    --weight_decay 5e-5 \
+    --transform 'imagenet' \
+    --lr 0.1 \
+    --eval_funcs 'v2' \
+    --warmup_teacher_temp 0.07 \
+    --teacher_temp 0.04 \
+    --warmup_teacher_temp_epochs 30 \
+    --memax_weight 4 \
+    --exp_name cifar100_simgcd
